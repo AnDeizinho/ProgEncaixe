b64445ae236b0c63b087cd8b1f6f7ec473bb8f8f
andrey <queirozandrey5@gmail.com>
2021-11-18 17:28:56 -0400

    atualizacao

---
+++ NOVEMBRO/COLETE/CALC.py
@@ -0,0 +1,14 @@
+Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:52:53) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> TAMANHO_P_CINTURA = 38
+>>> TAMANHO_G_SUBLIMADO = 100
+>>> TECIDO_TOTAL = 300
+>>> TECIDO_SUBLIMADO_G = 100 * 0.25
+>>> TECIDO_CINTURA_P = 38 * 0.24
+>>> DIFERENCA = TECIDO_TOTAL - TECIDO_SUBLIMADO_G - TECIDO_CINTURA_P
+>>> DIFERENCA
+265.88
+>>> CAP_DIF = DIFERENCA / 0.51
+>>> CAP_DIF
+521.3333333333334
+>>> 
